ca6ff30120f662000d1f7ee1b1f4f071902fcce1
Vladimir Belitskiy <belitskiy@gmail.com>
2018-12-12 10:58:17 -0500

    Make from_divs_to_dict stop parsing watch-history once it runs into content from previous watch-history file, if present
    Add a function for adding newlines again

--- convert_takeout.py
+++ convert_takeout.py
@@ -1,28 +1,50 @@
 from bs4 import BeautifulSoup as BSoup
 import os
 import re
+import json
+from ktools.fn import extract_num_from_filename
 from config import WORK_DIR
+
 """
 Turns out the reason for BS not seeing all/any divs was an extra,
 out-of-place tag - <div class="mdl-grid"> (right after <body>) instead of 
 indentation and newlines. Thought BeautifulSoup was supposed to handle that 
 just fine, but maybe not or not in all cases. 
 """
-os.chdir(WORK_DIR)
 
-original_html = os.path.join(WORK_DIR, 'watch-history.html')
+WORK_DIR = os.path.join(WORK_DIR, 'takeout_data')
+os.chdir(WORK_DIR)
 watch_url = re.compile(r'watch\?v=')
 
 
 def from_divs_to_dict(path, silent=False):
-    """Retrieves all the divs with data and dumps said date as JSON"""
-    with open(path, 'r', encoding='utf-8') as file:
-        soup = BSoup(file, 'lxml')
+    """Retrieves video ids and timestamps (when they were watched) and
+    returns them in a dict"""
+    with open(path, 'r', encoding='utf-8') as json_watched:
+        soup = BSoup(json_watched, 'lxml')
 
+    occ_dict = {}
     divs = soup.find_all(
         'div',
         class_='content-cell mdl-cell mdl-cell--6-col mdl-typography--body-1')
-    occ_dict = {}
+
+    last_watched_now = None
+    last_watched = None
+    for str_ in divs[0].stripped_strings:
+        last_watched_now = str_.strip()
+    cur_file_num = extract_num_from_filename(path, True)
+
+    with open('first_videos_in_list.json', 'r') as json_watched:
+        json_watched = json_watched.read()
+        if json_watched != '':
+            last_watched_dict = json.loads(json_watched)
+            if str(cur_file_num-1) in last_watched_dict:
+                last_watched = last_watched_dict[str(cur_file_num-1)]
+        else:
+            last_watched_dict = {}
+    with open('first_videos_in_list.json', 'w') as new_json_watched:
+        last_watched_dict[str(cur_file_num)] = last_watched_now
+        json.dump(last_watched_dict, new_json_watched, indent=4)
     for div in divs:
 
         url = div.find(href=watch_url)
@@ -35,6 +57,9 @@ def from_divs_to_dict(path, silent=False):
             watched_on = ''
             for str_ in div.stripped_strings:
                 watched_on = str_.strip()
+            if watched_on == last_watched:
+                print('!')
+                break
 
             occ_dict[video_id].append(watched_on)
 
@@ -42,8 +67,10 @@ def from_divs_to_dict(path, silent=False):
     pprint(sorted([[k, v] for k, v in occ_dict.items()],
                   key=lambda entry: len(entry[1]))[-1])
     if not silent:
-        print('Total videos watched:', len(divs))
-        print('Unique videos watched:', len(occ_dict))
+        print('Total videos watched:', sum(
+            [len(vals) for vals in occ_dict.values()]))
+        print('Unique, new videos watched:', len(occ_dict))
+
     del divs
     return occ_dict
 
@@ -96,3 +123,25 @@ def clean_up_tags(path: str, new_path: str):
 # remove watched_on column from videos table, replace with watched
 # (number of # times)
 # create a table that lists all times watched for each video (one-to-many)
+
+
+def add_newlines(path: str):
+    with open(path, 'rb') as file, open(
+            path[:path.find('.')] + '_newlined.html', 'wb') as new_file:
+        while True:
+            read_piece = file.read(1)
+            if not read_piece:
+                break
+            # if read_piece == b'<':
+            #     new_file.write(b'\n')
+            #     new_file.write(read_piece)
+            new_file.write(read_piece)
+            if read_piece == b'>':
+                new_file.write(b'\n')
+
+
+if __name__ == '__main__':
+    # from_divs_to_dict(os.path.join(WORK_DIR, 'watch-history_001.html'))
+    # from_divs_to_dict(os.path.join(WORK_DIR, 'watch-history_002.html'))
+    pass
+    add_newlines(os.path.join(WORK_DIR, 'watch-history_002.html'))
