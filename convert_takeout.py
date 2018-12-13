d6535e0c9cf7a9e36d028fb36e8bc1956896e1e2
Vladimir Belitskiy <belitskiy@gmail.com>
2018-12-12 20:14:44 -0500

    Some more polish

--- convert_takeout.py
+++ convert_takeout.py
@@ -12,15 +12,19 @@ Turns out the reason for BS not seeing all/any divs was an extra,
 out-of-place tag - <div class="mdl-grid"> (right after <body>) instead of 
 indentation and newlines. Thought BeautifulSoup was supposed to handle that 
 just fine, but maybe not or not in all cases. 
+
+Update:
+Perhaps the above tag is not out of place, but is paired with a seemingly 
+out-of-place </div> at the every end of the file. Its presence still doesn't 
+make sense as divs with the same class also wrap every video entry individually.
 """
-join = os.path.join
-WORK_DIR = os.path.join(WORK_DIR, 'takeout_data')
-os.chdir(WORK_DIR)
+
+os.chdir(os.path.join(WORK_DIR, 'takeout_data'))
 watch_url = re.compile(r'watch\?v=')
 
 
 def get_video_id(url):
-    video_id = url['href'][url['href'].find('=') + 1:]
+    video_id = url[url.find('=') + 1:]
     id_end = video_id.find('&t=')
     if id_end > 0:
         video_id = video_id[:id_end]
@@ -29,20 +33,12 @@ def get_video_id(url):
 
 
 @timer
-def from_divs_to_dict(path, silent=False):
+def from_divs_to_dict(path, silent=False, occ_dict=None):
     """Retrieves video ids and timestamps (when they were watched) and
     returns them in a dict"""
-    with open(path, 'r', encoding='utf-8') as json_watched:
-        soup = BSoup(json_watched, 'lxml')
 
-    occ_dict = {}
-    divs = soup.find_all(
-        'div',
-        class_='awesome_class')
-    last_watched_now = None
-    last_watched = None
-    for str_ in divs[0].stripped_strings:
-        last_watched_now = str_.strip()
+    last_watched_timestamp_now = None
+    last_watched_timestamp = None
     cur_file_num = extract_num_from_filename(path, True)
 
     with open('first_videos_in_list.json', 'r') as json_watched:
@@ -50,47 +46,54 @@ def from_divs_to_dict(path, silent=False):
         if json_watched != '':
             last_watched_dict = json.loads(json_watched)
             if str(cur_file_num-1) in last_watched_dict:
-                last_watched = last_watched_dict[str(cur_file_num-1)]
+                last_watched_timestamp = last_watched_dict[str(cur_file_num-1)]
         else:
             last_watched_dict = {}
+    content = clean_and_trim_html(path)
+    if last_watched_timestamp:
+        content = content[:content.find(last_watched_timestamp)]
+        content = content[:content.rfind('<div')]  # since the previous
+        # find includes half a div from the previous watch-history.html
+    soup = BSoup(content, 'lxml')
+    count = 0
+    if occ_dict is None:
+        occ_dict = {}
+    divs = soup.find_all(
+        'div',
+        class_='awesome_class')
+    for str_ in divs[0].stripped_strings:
+        last_watched_timestamp_now = str_.strip()
     with open('first_videos_in_list.json', 'w') as new_json_watched:
-        last_watched_dict[str(cur_file_num)] = last_watched_now
+        last_watched_dict[str(cur_file_num)] = last_watched_timestamp_now
         json.dump(last_watched_dict, new_json_watched, indent=4)
     for div in divs:
-
         url = div.find(href=watch_url)
         if url:
-            video_id = get_video_id(url)
+            video_id = get_video_id(url['href'])
             occ_dict.setdefault(video_id, [])
-            watched_on = ''
+            watched_on = ''  # simply so PyCharm doesn't freak out,
+            # as if a video has a url, it'll also have a timestamp
             for str_ in div.stripped_strings:
-                watched_on = str_.strip()
-            if watched_on == last_watched:
-                print('!')
-                break
+                watched_on = str_
 
             occ_dict[video_id].append(watched_on)
+        count += 1
 
-    from pprint import pprint
-    pprint(sorted([[k, v] for k, v in occ_dict.items()],
-                  key=lambda entry: len(entry[1]))[-1])
     if not silent:
-        print('Total videos watched:', len(divs))
-        print('Total videos watched:', sum(
+        print('Total videos watched:', count)
+        print('Videos watched, with urls:', sum(
             [len(vals) for vals in occ_dict.values()]))
-        print('Unique, new videos watched:', len(occ_dict))
+        print('Unique videos watched:', len(occ_dict))
 
-    del divs
     return occ_dict
 
 
-@timer
-def keep_the_divs(path: str):
+def clean_and_trim_html(path: str):
     with open(path, 'r') as file:
         file = file.read()
     done_ = '<span id="Done">'
     if file.startswith(done_):
-        return
+        return file
     file = file[file.find('<body>')+6:file.find('</body>')-6]
     fluff = [  # the order should not be changed
         ('<div class="mdl-grid">', ''),
@@ -117,13 +120,23 @@ def keep_the_divs(path: str):
     with open(path, 'w') as new_file:
         new_file.write(file)
 
+    return file
 
-# todo convert this to work on multiple files? Populate into DB
+
+def get_all_vids():
+    watch_files = [file for file in os.listdir('.')
+                   if file.startswith('watch-history_')]
+    o_dict = {}
+    for file in watch_files:
+        from_divs_to_dict(file, occ_dict=o_dict)
+
+
+# todo Populate into DB?
 # adapt functions using this
 # remove watched_on column from videos table, replace with watched
 # (number of # times)
 # create a table that lists all times watched for each video (one-to-many)
 
 if __name__ == '__main__':
-    keep_the_divs('watch-history_001.html')
-    from_divs_to_dict(os.path.join(WORK_DIR, 'watch-history_001.html'))
+    pass
+    get_all_vids()
