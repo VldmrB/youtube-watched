a403667553f64c1e3a9684dd874ad30419305a24
Vladimir Belitskiy <belitskiy@gmail.com>
2019-01-03 20:58:56 -0500

    Another commit

--- static/simple.js
+++ static/simple.js
@@ -1,14 +1,8 @@
 var showDataDirForm = function(){
-    var form = document.getElementById('change_form');
+    var form = document.getElementById('data_dir_form');
     var show_form_btn = document.getElementById('show_form_button');
     var input_field = document.getElementById('data_dir_input_field');
-    if (form.style.display == 'none') {
-        form.style.display = 'block';
-        input_field.focus();
-        show_form_btn.innerHTML = 'Cancel';
-    } else {
-        form.style.display = 'none';
-        show_form_btn.style.display = 'inline';
-        show_form_btn.innerHTML = 'Change...';
-    }
+    form.className = '';
+    input_field.focus();
+    show_form_btn.style.display = 'none';
 };
