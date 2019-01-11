8c42fc91d09309cd594929d2c2eb7750fb24eacd
Vladimir Belitskiy <belitskiy@gmail.com>
2019-01-10 22:09:03 -0500

    Another commit

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import os
-import shutil
-import json
-# import logging
-# from utils import logging_config
-# import write_to_sql
-from utils import load_file
 from os.path import join
 from flask import Flask, render_template, url_for
 from flask import request, redirect, make_response, flash
+from convert_takeout import get_all_records
 
-PROFILES_JSON = 'profiles.json'
 app = Flask(__name__)
 app.secret_key = '23lkjhv9z8y$!gffflsa1g4[p[p]'
 
@@ -21,52 +15,86 @@ def strong(text):
     return f'<strong>{text}</strong>'
 
 
+def get_project_dir_path_from_cookie():
+    return request.cookies.get('project-dir')
+
+
 @app.route('/')
 def index():
-    path = request.cookies.get('project-dir')
-    if not path:
+    project_path = get_project_dir_path_from_cookie()
+    if not project_path:
         return render_template('index.html')
-    elif not os.path.exists(path):
-        flash(f'{flash_err} could not find directory {strong(path)}')
+    elif not os.path.exists(project_path):
+        flash(f'{flash_err} could not find directory {strong(project_path)}')
         return render_template('index.html')
 
-    return render_template('index.html', path=path)
+    api_key_path = join(project_path, 'api_key')
+    if os.path.exists(api_key_path):
+        with open(api_key_path, 'r') as api_file:
+            api_key = True if api_file.read().strip() else None
+    else:
+        api_key = None
+
+    return render_template('index.html', path=project_path, api_key=api_key)
 
 
 @app.route('/create_project_dir', methods=['POST'])
-def setup_project_dir():
-    if request.method == 'POST':
-        path = request.form['project-dir']
-        try:
-            os.makedirs(path, exist_ok=True)
-        except FileNotFoundError:
-            raise
-        except OSError:
-            flash(f'{flash_err} invalid path.')
-            return redirect(url_for('index'))
-        resp = make_response(redirect(url_for('index')))
-        if path:
-            dirs_to_make = ['logs', 'graphs']
-            try:
-                if not os.path.exists(path):
-                    os.mkdir(path)
-                for dir_ in dirs_to_make:
-                    os.mkdir(join(path, dir_))
-            except FileExistsError:
-                pass
-            except OSError:
-                flash(f'{flash_err} {strong(path)} is not a valid profile name')
-            resp.set_cookie('project-dir', path, max_age=31_536_000)
-            return resp
+def create_project_dir():
+    project_path = request.form['project-dir']
+    resp = make_response(redirect(url_for('index')))
+
+    try:
+        if not os.path.exists(project_path):
+            os.makedirs(project_path)
+            flash(f'{flash_note} '
+                  f'Created directory {os.path.abspath(project_path)}')
+        dirs_to_make = ['logs', 'graphs']
+        for dir_ in dirs_to_make:
+            os.mkdir(join(project_path, dir_))
+    except FileNotFoundError:
+        raise
+    except FileExistsError:
+        pass
+    except OSError:
+        flash(f'{flash_err} {strong(project_path)} is not a valid path')
+        return redirect(url_for('index'))
+    resp.set_cookie('project-dir', project_path, max_age=31_536_000)
+    return resp
+
+
+@app.route('/setup_api_key', methods=['POST'])
+def setup_api_key():
+    api_key = request.form['api-key']
+    project_path = get_project_dir_path_from_cookie()
+    with open(join(project_path, 'api_key'), 'w') as api_file:
+        api_file.write(api_key)
+
     return redirect(url_for('index'))
 
 
+@app.route('/convert_takeout', methods=['POST'])
+def convert_takeout():
+    takeout_path = request.form.get('takeout-path')
+    if not os.path.isdir(takeout_path):
+        flash(f'{flash_err} {strong(takeout_path)} is not a directory or a '
+              f'valid path.')
+        return redirect(url_for('index', failed=True))
+
+    takeout_records = get_all_records(takeout_path, silent=True)
+    if not takeout_records:
+        flash(f'{flash_err} No watch-history files found.')
+        return redirect(url_for('index', failed=True))
+
+    project_path = get_project_dir_path_from_cookie()
+
+    return redirect(url_for('index'))
+
+
+
 if __name__ == '__main__':
-    test_db_path = r'G:\pyton\youtube_watched_data\test2'
-    # os.chdir(test_db_path)
-    # log_path = r'logs\sql_fails.log'
-    # logging_config(log_path)
-    # logger = logging.getLogger(__name__)
+    app.run()
+    # from utils import logging_config
+    # logging_config(r'C:\Users\Vladimir\Desktop\sql_fails.log')
     # write_to_sql.setup_db()
 
     # write_to_sql.create_all_tables(test_db_path)
@@ -75,7 +103,4 @@ if __name__ == '__main__':
     # insert_categories(test_db_path, takeout_path[:takeout_path.rfind('\\')])
     # insert_parent_topics(test_db_path)
     # insert_sub_topics(test_db_path)
-    #
     # write_to_sql.insert_videos()
-
-    app.run()
