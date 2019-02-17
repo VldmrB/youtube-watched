7dc79255c6b012b0569022b8b98c2ee366bab159
Vladimir Belitskiy <belitskiy@gmail.com>
2019-02-16 23:04:45 -0500

    - In add_tags_to_table_and_video, implement check for adding tags to  the database, which was left out accidentally earlier, leading to  duplicate tags being added under different ids  - Change the tag column in the tags table to be unique  - In update_videos, fix a check for presence of topics for a video  while attempting to update the topics' list for it  - Start working on a function for tag related data visualisation

--- analyze.py
+++ analyze.py
@@ -1,5 +1,6 @@
 import os
-import json
+import sqlite3
+from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -7,13 +8,10 @@ from scipy.interpolate import interp1d
 from ktools import utils
 from testing import WORK_DIR
 from data_prep import duplicate_tags
-
-# Nov 19, 2018, 11:07:59 PM EST
-# %b  %d,   %Y, %I:%M:%S %p  %Z
-# https://www.youtube.com/watch?v=Pd3P8L-EIAo
+from sql_utils import execute_query
 
 
-def refine(x: np.array, y: np.array, fine: int, kind: str='quadratic'):
+def refine(x: np.array, y: np.array, fine: int, kind: str = 'quadratic'):
     x_refined = np.linspace(x.min(), x.max(), fine)
     func = interp1d(x, y, kind=kind)
     y_refined = func(x_refined)
@@ -35,33 +33,28 @@ def plot_data(data: dict, save_path=None):
 
 
 @utils.timer
-def plot_tags():
-    # TODO fix this
-    records = {'PLACEHOLDER': 'DICT! FIX THIS FIRST'}
-    all_tags = {}
-    no_tags_dict = {}
-    for record in records:
-        try:
-            json_str = json.loads(record[1])['items'][0]['snippet']['tags']
-            for tag in json_str:
-                tag = tag.lower()
-                if tag == 'the':
-                    continue
-                for k, v in duplicate_tags.items():
-                    if tag in v:
-                        all_tags.setdefault(k, 0)
-                        all_tags[tag] += 1
-                        break
-                else:
-                    all_tags.setdefault(tag, 0)
-                    all_tags[tag] += 1
-        except KeyError:
-            # no tags key for whatever reason
-            json_str = json.loads(record[1])
-            no_tags_dict[json_str['items'][0]['id']] = json_str[
-                'items'][0]['snippet']['title']
-    p_series = pd.Series(all_tags).sort_values()[-50:]
-    # p_series = p_series[p_series > 25]
-    # p_series = p_series.filter(like='gam', axis=0)
+def plot_tags(conn: sqlite3.Connection):
+    results = execute_query(conn,
+                            '''SELECT t.tag FROM
+                            tags t JOIN videos_tags vt ON t.id = vt.tag_id
+                            ORDER BY t.tag;
+                            ''')
+    all_tags = Counter([i[0] for i in results])
+    deduplicated_tags = {}
+
+    for tag, tag_count in all_tags.items():
+        all_tags[tag] = tag.lower()
+        if tag == 'the':
+            continue
+        for k, v in duplicate_tags.items():
+            if tag in v:
+                deduplicated_tags.setdefault(k, 0)
+                deduplicated_tags[k] += tag_count
+                break
+        else:
+            deduplicated_tags.setdefault(tag, 0)
+            deduplicated_tags[tag] += tag_count
+    p_series = pd.Series(deduplicated_tags).sort_values()
+    p_series = p_series.filter(like='piano', axis=0)
     p_series.plot(kind='barh')
     plt.show()
