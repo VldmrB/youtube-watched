fe5ffba112a89a6caa64e6f0acaecb81032504ca
Vladimir Belitskiy <belitskiy@gmail.com>
2019-04-02 14:00:20 -0400

    Remove unused code from config.py

--- config.py
+++ config.py
@@ -1,29 +1,22 @@
-import os
 from datetime import timedelta
-
-if os.path.exists('api_key'):
-    with open('api_key', 'r') as file:
-        DEVELOPER_KEY = file.read()
-        if not DEVELOPER_KEY:
-            DEVELOPER_KEY = None
-else:
-    DEVELOPER_KEY = None
 
 DB_NAME = 'yt.sqlite'
 
-video_keys_and_columns = ('id', 'publishedAt',
-                          'channelId', 'title',
-                          'description',
-                          'channelTitle', 'tags',
-                          'categoryId',
-                          'defaultAudioLanguage',
-                          'duration',
-                          'viewCount',
-                          'likeCount',
-                          'dislikeCount',
-                          'commentCount',
-                          'relevantTopicIds',
-                          'actualStartTime')
+video_keys_and_columns = (
+    'id', 'publishedAt',
+    'channelId', 'title',
+    'description',
+    'channelTitle', 'tags',
+    'categoryId',
+    'defaultAudioLanguage',
+    'duration',
+    'viewCount',
+    'likeCount',
+    'dislikeCount',
+    'commentCount',
+    'relevantTopicIds',
+    'actualStartTime'
+)
 
 video_parts_to_get = ','.join([
     "contentDetails",  # 2
@@ -34,10 +27,10 @@ video_parts_to_get = ','.join([
     "liveStreamingDetails"  # 2
 ])
 
-# YouTube Takeout seems to return timestamps in local time,
-# but without a concrete timezone.
-# In case archives were downloaded in different parts of the world,
-# the same timestamp may show up as multiple different ones. The below value is
-# used by a function to eliminate/block such duplicates, though at a
-# potential cost of removing very few legitimate timestamps as well
+'''YouTube Takeout seems to return timestamps in local time,
+but without a concrete timezone.
+In case archives were downloaded in different parts of the world, the same 
+timestamp may show up as multiple different ones. The below value is used by a 
+function to eliminate/block such duplicates, though at a potential cost of 
+removing very few legitimate timestamps as well'''
 MAX_TIME_DIFFERENCE = timedelta(hours=25)
