0efd1db419287df23b027865b7d6f448dc6c1948
Vladimir Belitskiy <belitskiy@gmail.com>
2019-04-06 12:23:44 -0400

    - Allow main.py to start the server when called directly as a regular  script; move the dash_app import into the function that starts the  server as its Click's --help command takes way too long otherwise when  called from a console  - Remove the DEBUG variable from config.py, useless right now

--- youtubewatched/main.py
+++ youtubewatched/main.py
@@ -1,11 +1,16 @@
 import click
 
 from youtubewatched.config import PORT
-from youtubewatched.dash_layout import dash_app
 
 
 @click.command()
 @click.option('-p', '--port', default=PORT,
               help='The port at which the server will listen', type=click.INT)
 def launch(port):
+    # import is here as the --help command takes way too long otherwise
+    from youtubewatched.dash_layout import dash_app
     dash_app.run_server(port=port, debug=True)
+
+
+if __name__ == '__main__':
+    launch()
