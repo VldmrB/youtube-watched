905c7c72f5795008139482c94297097734c40df1
Vladimir Belitskiy <belitskiy@gmail.com>
2019-04-07 08:50:17 -0400

    Condense logging configuration and setting into fewer places; add code to disable the werkzeug logger (option to turn it back on and log its server requests messages to file is available) specifically as it does not seem to obey the laws of regular, mortal loggers

--- youtubewatched/main.py
+++ youtubewatched/main.py
@@ -5,7 +5,7 @@ from youtubewatched.config import PORT
 
 @click.command()
 @click.option('-p', '--port', default=PORT,
-              help='The port at which the server will listen', type=click.INT)
+              help='Server port (default: 5000)', type=click.INT)
 def launch(port):
     # import is here as the --help command takes way too long otherwise
     from youtubewatched.dash_layout import dash_app
