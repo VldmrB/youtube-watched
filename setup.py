a45bc8b8880f42893039275f33315c583cddcfb6
Vladimir Belitskiy <belitskiy@gmail.com>
2019-04-08 16:51:43 -0400

    In setup.py:  - add a custom 'upload' command to setup.py that clears out the dist/,  directory if any, builds sdist and bdist_wheel and uploads them to PyPi  (test version for now). Based on Kenneth Reitz's setup.py on GH  - explicitly convert the directory path retrieved from the __file__  attribute to an absolute as it may return a relative path under certain  circumstances
     Rename main.py to __main__.py to allow starting the server in debugging
     mode https://stackoverflow.com/a/25504196/7432972

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
+import subprocess
+import sys
 from os import path
-from setuptools import setup, find_packages
+from shutil import rmtree
+
+from setuptools import setup, find_packages, Command
 
 NAME = 'youtubewatched'
 
@@ -14,17 +18,50 @@ requires = [
     'plotly==3.7.1'
 ]
 
-with open(path.join(path.dirname(__file__), 'README.md'), 'r') as readme:
-    long_description = readme.read()
+cur_dir = path.abspath(path.dirname(__file__))
+
+
+class UploadCommand(Command):
+
+    # base on https://github.com/kennethreitz/setup.py/blob/master/setup.py
+    description = 'Build and publish the package.'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def run(self):
+        dist_dir_path = path.join(cur_dir, 'dist')
+        if path.exists(dist_dir_path):
+            print('Removing old dist/ directory')
+            try:
+                if not self.dry_run:
+                    rmtree(dist_dir_path)
+            except OSError:
+                print('Failed to delete the dist/ directory')
+                raise
+
+        print('Building source distribution and wheel')
+        if self.dry_run:
+            subprocess.run(f'{sys.executable} setup.py -n sdist bdist_wheel')
+        else:
+            subprocess.run(f'{sys.executable} setup.py sdist bdist_wheel')
+
+        print('Uploading to test PyPi...')
+        if not self.dry_run:
+            subprocess.run(f'twine upload --repository testpypi dist/*')
+
+    def finalize_options(self):
+        pass
+
 
 setup(
     name=NAME,
-    version='0.1.0',
+    version='0.1.0rc1',
     author='Vladimir Belitskiy',
     author_email='belitskiy@gmail.com',
-    description='Visualization of Youtube watch history from Google '
-                'Takeout',
-    long_description=long_description,
+    description='Visualization of Youtube watch history from Google Takeout',
+    long_description=open(path.join(cur_dir, 'README.md'), 'r').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/VldmrB/youtube-watched',
     packages=find_packages(),
@@ -45,5 +82,5 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Framework :: Flask'
     ],
+    cmdclass={'upload': UploadCommand}
 )
-
--- youtubewatched/__main__.py
+++ youtubewatched/__main__.py
@@ -4,11 +4,11 @@ from youtubewatched.config import PORT
 
 
 @click.command()
-@click.option('-d', '--debug', is_flag=True,
+@click.option('-d', '--debug', 'debug', is_flag=True,
               help='Enable debugging mode (Flask)')
 @click.option('-p', '--port', default=PORT,
               help=f'Server port (default: {PORT})')
-def launch(port, debug):
+def launch(debug, port):
     # import is here as the --help command takes way too long otherwise
     from youtubewatched.dash_layout import dash_app
     dash_app.run_server(port=port, debug=debug)
